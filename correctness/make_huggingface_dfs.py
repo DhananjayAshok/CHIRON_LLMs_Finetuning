442d5c571363ad8e52a1d64b195ab7593b6cce8e
Dhananjay Ashok <dhananjay.ashok99@gmail.com>
2024-08-07 18:50:51 -0700

    Added multi train configurations

--- correctness/make_huggingface_dfs.py
+++ correctness/make_huggingface_dfs.py
@@ -11,7 +11,7 @@ def proc_df(df, list_cols):
 def expand_df(df):
     cicero_path = "../dataset/human_game/Cicero_orders_dataset/"
     data = []
-    columns = ["message", "speaker", "receiver", "sender_label", "receiver_label", "game_score_delta", "time_code", "cicero_orders", "game_id","absolute_message_index","relative_message_index"]
+    columns = ["message", "message_history", "speaker", "receiver", "sender_label", "receiver_label", "game_score_delta", "time_code", "cicero_orders", "game_id","absolute_message_index","relative_message_index"]
     for i, row in tqdm(df.iterrows(), total=len(df)):
         for j in range(len(row["messages"])):
             timecode = eval(row["seasons"])[j][0]+eval(row["years"])[j]
@@ -35,7 +35,10 @@ def expand_df(df):
                         cicero_orders = ""
                     else:
                         cicero_orders = report_move(row["speakers"][j].upper(), moves)
-            data.append([row["messages"][j], row["speakers"][j], row["receivers"][j], row["sender_labels"][j], row["receiver_labels"][j], eval(row['game_score_delta'])[j], timecode, cicero_orders, row["game_id"], row['absolute_message_index'], row["relative_message_index"]])
+            history = []
+            for k in range(j):
+                history.append(row["speakers"][k] + ": "+ row["messages"][k])
+            data.append([row["messages"][j], history, row["speakers"][j], row["receivers"][j], row["sender_labels"][j], row["receiver_labels"][j], eval(row['game_score_delta'])[j], timecode, cicero_orders, row["game_id"], row['absolute_message_index'], row["relative_message_index"]])
     return pd.DataFrame(data, columns = columns)
 
 def get_dataset():
@@ -51,19 +54,31 @@ def get_dataset():
     val = expand_df(val)
     return train, test, val
 
-def construct_hf_data(df):
-    df["sentence"] = df["speaker"] + " has score difference of" + df["game_score_delta"].astype(str)+ " and " + df["cicero_orders"] + " sends " + df["receiver"] + ": "+df["message"]
-    df["sentence"] = df["speaker"] + " sends " + df["receiver"] + ": "+df["message"]
+def construct_hf_datas(df):
     df["label"] = (df["sender_label"] == False) | (df["receiver_label"] == False).astype(int)
-    return df[['sentence', 'label']]
+    df["m_text"] = df["speaker"] + " sends " + df["receiver"] + ": "+df["message"]
+    df["s_text"] = "score difference: " + df["game_score_delta"].astype(str)
+    df["c_text"] = df["speaker"] + "will move " + df["cicero_orders"]
+    df["h_text"] = "history: " + df["message_history"].apply(lambda x: " ".join(x))
+    variants = {"m": None, "ms": None, "msc": None, "msch": None}
+    df["sentence"] = df["m_text"]
+    variants["m"] = df[["sentence", "label"]]
+    df["sentence"] = df["s_text"] + "|" + df["m_text"]
+    variants["ms"] = df[["sentence", "label"]]
+    df["sentence"] = df["c_text"] + "|" + df["s_text"] + "|" + df["m_text"]
+    variants["msc"] = df[["sentence", "label"]]
+    df["sentence"] = df["h_text"] + "|" + df["c_text"] + "|" + df["s_text"] + "|" + df["m_text"]
+    variants["msch"] = df[["sentence", "label"]]
+    return variants
 
 if __name__ == "__main__":
     print(f"This script should be run from the correctness folder (not root) or it will cause bugs")
     train, test, val = get_dataset()
-    train = construct_hf_data(train)
-    test = construct_hf_data(test)
-    val = construct_hf_data(val)
-    train.to_csv('../dataset/intention/train_hf.csv', index=False)
-    test.to_csv('../dataset/intention/test_hf.csv', index=False)
-    val.to_csv('../dataset/intention/validation_hf.csv', index=False)
-
+    trains = construct_hf_datas(train)
+    tests = construct_hf_datas(test)
+    vals = construct_hf_datas(val)
+    base_path = "../dataset/intention/"
+    for key in trains.keys():
+        trains[key].to_csv(base_path + f"train_{key}.csv", index=False)
+        tests[key].to_csv(base_path + f"test_{key}.csv", index=False)
+        vals[key].to_csv(base_path + f"val_{key}.csv", index=False)
