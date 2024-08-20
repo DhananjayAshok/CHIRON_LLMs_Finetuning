4270d115d82399167f06a5e4d465b515f2a440f6
Dhananjay Ashok <dhananjay.ashok99@gmail.com>
2024-08-20 10:20:24 -0700

    script fix

--- correctness/train_intention_classifier.sh
+++ correctness/train_intention_classifier.sh
@@ -1,4 +1,4 @@
 model_name=llama3
-common="--num_train_epochs 2 --learning_rate 5e-5 --model_name_or_path eta-llama/Meta-Llama-3.1-8B --peft lora"
+common="--num_train_epochs 3 --learning_rate 5e-5 --model_name_or_path meta-llama/Meta-Llama-3.1-8B --peft lora"
 data_root=../dataset/intention
-python classification_finetune.py --max_seq_len 180 --train_file $data_root/train_og_score.csv --validation_file $data_root/validation_og_score.csv --output_dir models/"$model_name"_og_score $common
+python llm-utils/classification_finetune.py --max_seq_len 180 --train_file $data_root/train_og_score.csv --validation_file $data_root/validation_og_score.csv --output_dir models/"$model_name"_og_score $common
